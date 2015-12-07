c92c012bed31b193a74cce22e5e97ddf9a73ee2c
Guillermo García Galindo <gekubo@gmail.com>
2015-12-07 23:10:37 +0100

    Online CV

--- js/vendor/jquery.ui.totop.js
+++ js/vendor/jquery.ui.totop.js
@@ -9,7 +9,7 @@
 
  		var defaults = {
     			text: 'To Top',
-    			min: 200,
+    			min: 800,
     			inDelay:600,
     			outDelay:400,
       			containerID: 'toTop',
@@ -20,7 +20,7 @@
             settings = $.extend(defaults, options),
             containerIDhash = '#' + settings.containerID,
             containerHoverIDHash = '#'+settings.containerHoverID;
-		
+
 		$('body').append('<a href="#" id="'+settings.containerID+'">'+settings.text+'</a>');
 		$(containerIDhash).hide().on('click.UItoTop',function(){
 			$('html, body').animate({scrollTop:0}, settings.scrollSpeed, settings.easingType);
@@ -32,12 +32,12 @@
 				$(containerHoverIDHash, this).stop().animate({
 					'opacity': 1
 				}, 600, 'linear');
-			}, function() { 
+			}, function() {
 				$(containerHoverIDHash, this).stop().animate({
 					'opacity': 0
 				}, 700, 'linear');
 			});
-					
+
 		$(window).scroll(function() {
 			var sd = $(window).scrollTop();
 			if(typeof document.body.style.maxHeight === "undefined") {
@@ -46,9 +46,9 @@
 					'top': sd + $(window).height() - 50
 				});
 			}
-			if ( sd > settings.min ) 
+			if ( sd > settings.min )
 				$(containerIDhash).fadeIn(settings.inDelay);
-			else 
+			else
 				$(containerIDhash).fadeOut(settings.Outdelay);
 		});
 };
